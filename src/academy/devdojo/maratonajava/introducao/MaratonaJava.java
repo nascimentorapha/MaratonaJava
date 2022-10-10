67b33dbe4566075e6a8f2c28afb3bb39d0d05ccc
raphael.nascimento <nascimento.rapha@hotmail.com>
2022-10-09 21:10:56 -0300

    Javadoc test

--- src/academy/devdojo/maratonajava/introducao/MaratonaJava.java
+++ src/academy/devdojo/maratonajava/introducao/MaratonaJava.java
@@ -1,5 +1,9 @@
 package academy.devdojo.maratonajava.introducao;
 
+/**
+ * Teste de <b>javadoc</b>.
+ */
+
 public class MaratonaJava {
     public static void main(String[] args) {
         System.out.println("KA-ME-HA-ME-HAAA!");
