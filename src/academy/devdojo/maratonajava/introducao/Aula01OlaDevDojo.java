37aa515a6a6a1df60b345816cc49955dea7fa7f6
raphael.nascimento <nascimento.rapha@hotmail.com>
2022-10-09 21:18:26 -0300

    Refactored class to correctly order

--- src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
+++ src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -4,7 +4,7 @@ package academy.devdojo.maratonajava.introducao;
  * Teste de <b>javadoc</b>.
  */
 
-public class MaratonaJava {
+public class Aula01OlaDevDojo {
     public static void main(String[] args) {
         System.out.println("KA-ME-HA-ME-HAAA!");
     }
