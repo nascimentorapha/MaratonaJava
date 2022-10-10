d536326357426ae6595f785bc3407092cb4c025f
raphael.nascimento <nascimento.rapha@hotmail.com>
2022-10-09 21:38:38 -0300

    Class 2 - primitive types of java

---
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // tipos primitivos = int, double, float, char, byte, short, long, boolean
+        int age = 27;
+        System.out.println("A idade é "+age+" anos");
+    }
+}
