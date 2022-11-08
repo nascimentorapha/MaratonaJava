cc7fbd05890f19373e5f2ace6bc55ae2426a5332
Raphael Nascimento <nascimento.rapha@hotmail.com>
2022-11-08 17:35:28 -0300

    primitive types class

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -4,6 +4,17 @@ public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // tipos primitivos = int, double, float, char, byte, short, long, boolean
         int age = 27;
+        long numeroGrande = 100000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = '\u0041';
+
         System.out.println("A idade é "+age+" anos");
+        System.out.println(verdadeiro);
+        System.out.println("char "+caractere);
     }
 }
