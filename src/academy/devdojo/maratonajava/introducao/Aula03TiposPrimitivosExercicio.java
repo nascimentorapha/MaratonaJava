b3e75f0423fe77128c97a344c41315eee10f6454
raphael.nascimento <nascimento.rapha@hotmail.com>
2022-11-19 14:56:08 -0300

    exercise primitive types

---
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String nome = "Goku";
+        String endereco = "avenida paulista, 500";
+        double salario = 2500.35;
+        String data = "01/01/2000";
+        String relatorioPagamento = "Eu "+nome+", morando no endereço "+endereco+", confirmo que recebi o salário de "+salario+", na data "+data;
+        System.out.println(relatorioPagamento);
+    }
+}
