c81299fb6a4669b70a95502373d1470b73c4a003
Raphael Nascimento <raphael.nascimento@TITECHCST.local>
2022-11-23 10:56:10 -0300

    operators class pt2

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -5,7 +5,24 @@ public class Aula04Operadores {
         // + - / *
         int numero01 = 10;
         int numero02 = 20;
-        double resultado = numero01 / numero02;
+        double resultado = numero01 / (double) numero02;
         System.out.println(resultado);
+
+        // (operador de resto) = %
+        int resto = 21 % 2;
+        System.out.println(resto);
+
+        // < | > | <= | >= | == | != sempre retorna valores booleanos
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezIgualDez = 10 == 10;
+        boolean isDezDiferenteDez = 10 != 10;
+        System.out.println("isDezMaiorQueVinte "+isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte "+isDezMenorQueVinte);
+        System.out.println("isDezIgualVinte "+isDezIgualVinte);
+        System.out.println("isDezIgualDez "+isDezIgualDez);
+        System.out.println("isDezDiferenteDez "+isDezDiferenteDez);
+
     }
 }
