8f120be2ceacc2d4c40f36a5a55c9c5cab1e608d
Raphael Nascimento <nascimento.rapha@hotmail.com>
2022-11-25 15:42:25 -0300

    final class operators

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -38,5 +38,17 @@ public class Aula04Operadores {
         boolean isPlaystationCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
 
         System.out.println("isPlaystationCincoCompravel "+isPlaystationCincoCompravel);
+
+        // += -= *= /= %=
+        double bonus = 1800;
+        bonus += 1000;
+        bonus *= 2.5;
+        bonus /= 2;
+        System.out.println(bonus);
+
+        //
+        int contador = 0;
+        contador++;
+        System.out.println(contador);
     }
 }
