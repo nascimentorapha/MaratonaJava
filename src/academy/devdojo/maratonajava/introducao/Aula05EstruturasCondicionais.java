e39d032357dbd6430b64944c08b27b81773e99a4
raphael.nascimento <nascimento.rapha@hotmail.com>
2022-11-26 16:11:31 -0300

    removed if condition of java certification study

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -12,10 +12,6 @@ public class Aula05EstruturasCondicionais {
         if(!isAutorizadoComprarBebida){
             System.out.println("Não autorizado a comprar bebida alcóolica");
         }
-        boolean c = false;
-        if (c = true){
-            System.out.println("Dentro de algo que nunca deve ser feito");
-        }
 
         System.out.println("Fora do if");
     }
