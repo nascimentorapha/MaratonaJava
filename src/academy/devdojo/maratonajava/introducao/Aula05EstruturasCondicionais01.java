2b035ea52b24ac7a649d1d59b2e85a6e6c36d000
Raphael Nascimento <nascimento.rapha@hotmail.com>
2022-11-28 15:37:12 -0300

    class 01 refactored. class 2 created

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais01.java
@@ -1,15 +1,18 @@
 package academy.devdojo.maratonajava.introducao;
 
-public class Aula05EstruturasCondicionais {
+public class Aula05EstruturasCondicionais01 {
     public static void main(String[] args) {
         int idade = 15;
-        boolean isAutorizadoComprarBebida = idade >=18;
+        boolean isAutorizadoComprarBebida = idade >= 18;
         // !
         if (isAutorizadoComprarBebida) {
             System.out.println("Autorizado a comprar bebida alcóolica");
+        } else {
+            System.out.println("Não autorizado a comprar bebida alcóolica");
         }
 
-        if(!isAutorizadoComprarBebida){
+
+        if (!isAutorizadoComprarBebida) {
             System.out.println("Não autorizado a comprar bebida alcóolica");
         }
 
