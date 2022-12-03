8ed58fed7ddf541f28a2e925482b0a3f3d460298
raphael.nascimento <nascimento.rapha@hotmail.com>
2022-12-03 15:58:16 -0300

    repetition exercise: print all pair numbers

---
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao02.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao02 {
+    public static void main(String[] args) {
+        for (int count = 1; count < 1000; count ++) {
+            if (count % 2 == 0)
+                System.out.println(count);
+        }
+    }
+}
