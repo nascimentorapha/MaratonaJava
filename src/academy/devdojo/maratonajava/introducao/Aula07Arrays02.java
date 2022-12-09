e2580c3fecaf30f18fd406bf9251cb0c524da001
raphael.nascimento <nascimento.rapha@hotmail.com>
2022-12-08 22:18:33 -0300

    pull repo. Added pt2 array class with default values of variables

---
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays02 {
+    public static void main(String[] args) {
+        // byte, short, int, long, float e double iniciam com 0
+        // char inicia '\u0000'
+        // boolean inicia com false
+        // String e qualquer outro inicia null
+
+        String nomes[] = new String[3];
+
+        System.out.println(nomes[0]);
+        System.out.println(nomes[1]);
+        System.out.println(nomes[2]);
+    }
+}
