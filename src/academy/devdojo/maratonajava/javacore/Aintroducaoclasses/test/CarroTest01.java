016ab76e9bdab2169875cff5ac9c4e0f7c880c06
raphael.nascimento <nascimento.rapha@hotmail.com>
2022-12-13 22:01:23 -0300

    Carro method edited, object reference lesson

--- src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/CarroTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/CarroTest01.java
@@ -10,9 +10,12 @@ public class CarroTest01 {
         carro01.nome = "A5";
         carro01.modelo = "Audi";
         carro01.ano = 2018;
+        
         carro02.nome = "CLA200";
         carro02.modelo = "Mercedes Benz";
         carro02.ano = 2020;
+
+        carro01 = carro02;
 
         System.out.println("Carro: " + carro01.nome + ", modelo: " + carro01.modelo + ", ano: " + carro01.ano);
         System.out.println("Carro: " + carro02.nome + ", modelo: " + carro02.modelo + ", ano: " + carro02.ano);
