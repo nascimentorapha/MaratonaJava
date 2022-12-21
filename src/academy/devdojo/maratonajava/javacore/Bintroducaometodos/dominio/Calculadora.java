ff85a7f26a688e8d23b0d67c1fc961b5dee11888
raphael.nascimento <nascimento.rapha@hotmail.com>
2022-12-20 21:59:58 -0300

    Reference parameter lesson. Included alteraDoisNumeros method on Calculadora.java. Created CalculadoraTest04 class

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -2,31 +2,39 @@ package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
 
 public class Calculadora {
 
-    public void somaDoisNumeros(){
+    public void somaDoisNumeros() {
         System.out.println(10 + 10);
     }
 
-    public void subtraiDoisNumeros(){
+    public void subtraiDoisNumeros() {
         System.out.println(5 - 3);
     }
 
-    public void multiplicaDoisNumeros(int num1, float num2){
+    public void multiplicaDoisNumeros(int num1, float num2) {
         System.out.println(num1 * num2);
     }
 
-    public double divideDoisNumeros(double num1, double num2){
-        if (num2 == 0){
+    public double divideDoisNumeros(double num1, double num2) {
+        if (num2 == 0) {
             return 0;
         }
-        return num1/num2;
+        return num1 / num2;
     }
 
-    public void imprimeDivisaoDeDoisNumeros(double num1, double num2){
-        if (num2 == 0){
+    public void imprimeDivisaoDeDoisNumeros(double num1, double num2) {
+        if (num2 == 0) {
             System.out.println("Não existe divisão por 0!");
             return;
         } else {
             System.out.println(num1 / num2);
         }
+    }
+
+    public void alteraDoisNumeros(int numero1, int numero2) {
+        numero1 = 99;
+        numero2 = 33;
+        System.out.println("Dentro do alteraDoisNumeros:");
+        System.out.println("Num1:" + numero1);
+        System.out.println("Num2:" + numero2);
     }
 }
