ca7b90e5156ee5ca006e1ab501fab612a994c340
raphael.nascimento <nascimento.rapha@hotmail.com>
2022-12-23 14:45:30 -0300

    Static Boot block lesson. Added Anime and AnimeTest01 classes on Fmodificadorestatico package

---
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/dominio/Anime.java
@@ -0,0 +1,39 @@
+package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.dominio;
+
+public class Anime {
+    private String nome;
+    private static int[] episodios;
+
+    // Instance boot block. After attributes and before constructor
+    // It's good because always will execute this instance independly constructor
+    static {
+        System.out.println("Dentro do bloco de inicialização estático 1");
+        episodios = new int[100];
+        for (int i = 0; i < episodios.length; i++) {
+            episodios[i] = i + 1;
+        }
+    }
+
+    static {
+        System.out.println("Dentro do bloco de inicialização estático 2");
+    }
+
+    {
+        System.out.println("Dentro do bloco de inicialização não estático");
+    }
+
+    public Anime() {
+        for (int episodio : Anime.episodios) {
+            System.out.print(episodio + " ");
+        }
+        System.out.println();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int[] getEpisodios() {
+        return episodios;
+    }
+}
