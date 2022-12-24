35a5aaae70ca663a16ab67a1a30aec62a5dbf318
raphael.nascimento <nascimento.rapha@hotmail.com>
2022-12-24 18:20:19 -0300

    Association and objects arrays lesson. Added Gassociacao package and its classes

---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/test/JogadorTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.test;
+
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Jogador;
+
+public class JogadorTest01 {
+    public static void main(String[] args) {
+        Jogador jogador1 = new Jogador("Pelé");
+        Jogador jogador2 = new Jogador("Neymar");
+        Jogador jogador3 = new Jogador("Anthony");
+        Jogador[] jogadores = {jogador1, jogador2, jogador3};
+
+        for (Jogador jogador : jogadores) {
+            jogador.imprime();
+        }
+
+    }
+}
