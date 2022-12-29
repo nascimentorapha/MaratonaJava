0e03124cfb874c85b24857bffbf1f7e9a8a66b86
raphael.nascimento <nascimento.rapha@hotmail.com>
2022-12-29 15:49:50 -0300

    Inheritance lesson pt1. Added Hheranca package and it's classes

---
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/test/HerancaTest01.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.javacore.Hheranca.test;
+
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Endereco;
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Funcionario;
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Pessoa;
+
+public class HerancaTest01 {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua 3");
+        endereco.setCep("012345-209");
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Kilua Aya");
+        pessoa.setCpf("11111111110");
+        pessoa.setEndereco(endereco);
+        pessoa.imprime();
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Hisoka");
+        funcionario.setCpf("22222222233");
+        funcionario.setEndereco(endereco);
+        funcionario.setSalario(4500);
+
+        funcionario.imprime();
+    }
+}
